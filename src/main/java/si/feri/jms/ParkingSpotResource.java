03625b7c9bf7ce7d25348279c396619c782ad826
perkzen <perko.domen@gmail.com>
2024-03-21 15:53:58 +0100

    feat: add logger

--- src/main/java/si/feri/jms/ParkingSpotResource.java
+++ src/main/java/si/feri/jms/ParkingSpotResource.java
@@ -1,15 +1,19 @@
 package si.feri.jms;
 
 import si.feri.jms.consumer.ParkingConsumer;
+import si.feri.parkinglot.ParkingLotResource;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.logging.Logger;
 
 @Path("/park")
 public class ParkingSpotResource {
+
+    private final Logger log = Logger.getLogger(ParkingLotResource.class.getName());
 
     @Inject
     ParkingConsumer consumer;
@@ -19,6 +23,7 @@ public class ParkingSpotResource {
     @Path("last")
     @Produces(MediaType.TEXT_PLAIN)
     public String last() {
+        log.info("Getting last parking spot");
         return consumer.getLastParkingSpot();
     }
 }
--- src/main/java/si/feri/parkinglot/ParkingLotResource.java
+++ src/main/java/si/feri/parkinglot/ParkingLotResource.java
@@ -6,10 +6,13 @@ import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 @Path("/park")
 public class ParkingLotResource {
+
+    private final Logger log = Logger.getLogger(ParkingLotResource.class.getName());
 
     @Inject
     ParkingSpotRepository repository;
@@ -17,11 +20,13 @@ public class ParkingLotResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<String> getAll() {
+        log.info("Getting all parking spots");
         return repository.listAllParkingSpots().await().indefinitely().stream().map(ParkingSpot::getName).collect(Collectors.toList());
     }
 
     @POST
     public Uni<Void> add(CreateParkingSpotDto dto) {
+        log.info("Adding parking spot: %s".formatted(dto));
         return repository.add(dto.name, dto.location);
     }
 
@@ -29,6 +34,7 @@ public class ParkingLotResource {
     @Path("/{name}")
     @Produces(MediaType.APPLICATION_JSON)
     public ParkingSpot get(@PathParam("name") String name) {
+        log.info("Getting parking spot: %s".formatted(name));
         return repository.getParkingSpot(name).await().indefinitely();
     }
 
